b9f4750a08b6533b9bb0abc4df14389bdfcc3e9c
Richard Chapman <rchapman@hpccsystems.com>
2012-02-19 09:35:30 +0000

    Add export info to minmax graph
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- web/minmaxsource.sh
+++ web/minmaxsource.sh
@@ -15,8 +15,15 @@ DEF:PowerMin=${RRDFILE}:Power:MIN \
 DEF:PowerMax=${RRDFILE}:Power:MAX \
 CDEF:PowerRange=PowerMax,PowerMin,- \
 DEF:PowerGen=${RRDFILE}:generating:AVERAGE \
-AREA:PowerGen#00FF00:"Average" \
-AREA:Power#0000FF:"Average":STACK \
+DEF:PowerExport=${RRDFILE}:exporting:AVERAGE \
+CDEF:PowerGenClean=PowerGen,0,ADDNAN  \
+CDEF:PowerExpClean=PowerExport,0,ADDNAN  \
+CDEF:PowerGenUse1=PowerGenClean,PowerExpClean,-  \
+CDEF:PowerGenUse=PowerGenUse1,0,LT,0,PowerGenUse1,IF  \
+AREA:PowerGenUse#00FF00:"Average" \
+STACK:Power#0000FF:"Average" \
+AREA:PowerGenUse#00FF00:"Average" \
+STACK:PowerExport#00FF0040:"Average" \
 LINE1:PowerGen: \
 LINE1:PowerMin::STACK \
 AREA:PowerRange#FF000040:"Error Range":STACK \
--- web/simplesource.sh
+++ web/simplesource.sh
@@ -17,5 +17,6 @@ CDEF:PowerExpClean=PowerExport,0,ADDNAN  \
 CDEF:PowerGenUse1=PowerGenClean,PowerExpClean,-  \
 CDEF:PowerGenUse=PowerGenUse1,0,LT,0,PowerGenUse1,IF  \
 AREA:PowerGenUse#00FF00:"Average" \
-STACK:PowerExport#00FF0040:"Average" \
-STACK:Power#0000FF:"Average"
+STACK:Power#0000FF:"Average" \
+AREA:PowerGenUse#00FF00:"Average" \
+STACK:PowerExport#00FF0040:"Average" 
