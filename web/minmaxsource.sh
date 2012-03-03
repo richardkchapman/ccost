36d136e92309e4d48530ffe65605f8d735b322a3
Richard Chapman <rchapman@hpccsystems.com>
2012-03-03 10:22:37 +0000

    Fix min/max when exporting (still not perfect)
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- web/minmaxsource.sh
+++ web/minmaxsource.sh
@@ -20,14 +20,16 @@ CDEF:PowerGenClean=PowerGen,0,ADDNAN  \
 CDEF:PowerExpClean=PowerExport,0,ADDNAN  \
 CDEF:PowerGenUse1=PowerGenClean,PowerExpClean,-  \
 CDEF:PowerGenUse=PowerGenUse1,0,LT,0,PowerGenUse1,IF  \
+\
 AREA:PowerGenUse#00FF00:"Average" \
 STACK:Power#0000FF:"Average" \
-AREA:PowerGenUse#00FF00:"Average" \
-STACK:PowerExport#00FF0040:"Average" \
-LINE1:PowerGen: \
+LINE1:PowerGenUse: \
 LINE1:PowerMin::STACK \
 AREA:PowerRange#FF000040:"Error Range":STACK \
-LINE1:PowerGen: \
+LINE1:PowerGenUse: \
 LINE1:PowerMin#FF000060:"Min":STACK \
-LINE1:PowerGen: \
-LINE1:PowerMax#FF000060:"Max":STACK 
+LINE1:PowerGenUse: \
+LINE1:PowerMax#FF000060:"Max":STACK \
+\
+AREA:PowerGenUse#00FF00:"Average" \
+STACK:PowerExport#00FF0040:"Average" \
